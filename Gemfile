b842050e28c81604f062e17b551741df01e1b933
dak2 <dawt2h@gmail.com>
2024-07-07 15:57:18 +0900

    call current weather api

--- Gemfile
+++ Gemfile
@@ -8,3 +8,7 @@ gemspec
 gem "rake", "~> 13.0"
 
 gem "rubocop", "~> 1.21"
+
+gem "steep"
+
+gem "dotenv"
--- lib/otenki/cli.rb
+++ lib/otenki/cli.rb
@@ -1,11 +1,30 @@
+# frozen_string_literal: true
+
 require "otenki"
 require "thor"
+require "net/http"
+require "dotenv/load"
+require_relative "constants"
 
 module Otenki
   class CLI < Thor
-    desc "test NAME", "Test method to say hello to NAME"
-    def test(name)
-      puts "Hello, #{name}"
+    desc "Get the current weather for a specific city", "Pass city name to get weather information"
+    def current_weather(city_name)
+      uri = URI("#{Constants::WEATHER_URI}/current.json")
+      params = { key: ENV['WEATHER_API_KEY'], q: city_name }
+      uri.query = URI.encode_www_form(params)
+      res = Net::HTTP.get_response(uri)
+      puts res.body
+    rescue StandardError => e
+      puts "Error: #{e}"
     end
+
+    # def forecast_weather_upto_three_days(city_name, days = 1)
+    #   puts "Hello, #{city_name}"
+    # end
+
+    # def past_weather(city_name)
+    #   puts "Hello, #{city_name}"
+    # end
   end
 end
